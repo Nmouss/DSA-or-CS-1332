abc381994928a3a5c6dfb3e68feb87c7cdee2f74
Nmouss <97858405+Nmouss@users.noreply.github.com>
2024-10-22 16:36:57 -0400

    Update MergeSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -10,14 +10,14 @@ public class MergeSort{ // pretty basic sorting algorithm
     if (array.length <= 1){
       return array;
     }
-    int mid = array.legnth / 2;
+    int mid = array.length / 2;
     Integer[] left = new Integer[mid];
     Integer[] right = new Integer[array.length - mid];
     for (int i = 0; i < mid; i++){ // copying arrays from the first half to left (I could have used the Arrays class but lets do everything manually)
       left[i] = array[i];
     }
     for (int i = mid; i < length; i++){ // copying right half of the array
-      left[i - mid] = array[i]; // if i = 4 and mid = 3 then, left[1] = ar
+      right[i - mid] = array[i]; // if i = 4 and mid = 3 then, left[1] = ar
     }
     Integer[] left_half = mergesort(left);
     Integer[] right_half = mergesort(right);
