7bd1a0c4ac0a23cadaea4947b80e4c7d549968a7
Nmouss <97858405+Nmouss@users.noreply.github.com>
2024-10-22 18:52:43 -0400

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -1,26 +1,25 @@
-import java.util.*;
 public class QuickSort{
   public static void main(String[] args){
     Integer[] array = {4, 3, 6, 100, 8, 9, 54, 1, 0};
-    QuickSort(array);
+    quicksort(0, array.length - 1, array);
     System.out.println(Arrays.toString(array));
   }
   public static void quicksort(int left, int right, Integer[] array){
     if (left < right){
-      mid = helper(left right, array);
-      QuickSort(mid + 1, right, array);
-      QuickSort(left, mid - 1, array);
+      int mid = helper(left, right, array);
+      quicksort(mid + 1, right, array);
+      quicksort(left, mid - 1, array);
     }
   }
   public static int helper(int left, int right, Integer[] array){
     int l = left;
     int r = right;
-    pivot = array[right];
+    int pivot = array[right];
     while (l < r){
       while ((array[l] < array[right]) && (l < right)){
         l++;
       }
-      while ((array[r] <= array[right]) && (r > left)){
+      while ((array[r] >= array[right]) && (r > left)){
         r--;
       }
       if (l < r){
@@ -29,8 +28,8 @@ public class QuickSort{
         array[l] = swapVal;
       }
     }
-    if (array[l] < pivot){
-      Integer temp = array[l]; 
+    if (array[l] > pivot){
+      int temp = array[l]; 
       array[l] = pivot;
       array[right] = temp;
     }
