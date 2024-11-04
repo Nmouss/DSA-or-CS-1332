e09b4467a98f0178c0a414cbbe98c41c37d0dc70
Nmouss <97858405+Nmouss@users.noreply.github.com>
2024-11-04 14:12:43 -0500

    Update LinkedStack.java

--- LinkedStack.java
+++ LinkedStack.java
@@ -1,1 +1,15 @@
-
+public class LinkedStack<T>{
+  
+}
+class Node<T>{
+  Node next;
+  T data;
+  public Node(T data){
+    this.data = data;
+    this.next = null;
+  }
+  public Node(T data, Node<T> next){
+    this.data = data;
+    this.next = next;
+  }
+}
