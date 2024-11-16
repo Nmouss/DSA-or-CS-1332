8479efc4650a99ad5ba36ffcb830233a153ed9a5
Nmouss <97858405+Nmouss@users.noreply.github.com>
2024-11-16 18:35:38 -0500

    Create Node.java

---
+++ BinaryTrees/Node.java
@@ -0,0 +1,31 @@
+public class Node<T>{
+  private Node<T> left;
+  private Node<T> right;
+  private T data;
+  public Node(){
+    this{null);
+  }
+  public Node(T data){
+    this(data, null, null);
+  }
+  public Node(T data, Node<T> left, Node<T> right){
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+  public T getData(){
+    return data;
+  }
+  public Node<T> getLeft(){
+    return left;
+  }
+  public Node<T> getRight(){
+    return right;
+  }
+  public void setLeftNode(Node<T> node){
+    this.left = node;
+  }
+  public void setRightNode(Node<T> node){
+    this.right = node;
+  }
+}
