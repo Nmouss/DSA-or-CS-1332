d246f39ab2a84388b92536dfeaa537d745ef7afd
Nmouss <97858405+Nmouss@users.noreply.github.com>
2024-11-17 10:05:31 -0500

    Update BinarySearchTree.java

--- BinaryTrees/BinarySearchTree.java
+++ BinaryTrees/BinarySearchTree.java
@@ -1,7 +1,51 @@
-public class BinarySearchTree<T> implements {
+public class BinarySearchTree<T> implements BinarySearchTreeInterface<T>{
   Node<T> root;
   public BinarySearchTree(){
     root.setRootData(null);
   }
-  
+
+  public void clear(){
+    root = Node<T>();
+  }
+
+  public boolean isEmpty(){
+    if (root.getData() == null){
+      return true;
+    }
+    return false;
+  }
+
+  public boolean contains(T value){
+    return search(root, value);
+  }
+
+  public T findMin(){
+  }
+
+  public T findMax(){
+  }
+
+  public void add(T data){
+  }
+
+  public void delete(T data){
+  }
+
+  public void printTree(){
+  }
+
+  private boolean search(Node<T> root, T value){
+    if (root.getData() == null){
+      return false;
+    }
+    if (value.compareTo(root.getData()) > 0){
+      return search(root.getRight())
+    }
+    if (value.compareTo(root.getData()) < 0){
+      return search(root.getLeft())
+    }
+    if (value.compareTo(root.getData()) == 0){
+      return true;
+    }
+  }
 }
