b7091f497b8dce151228db10cef7ad7e7496be07
Nmouss <97858405+Nmouss@users.noreply.github.com>
2024-11-17 09:23:44 -0500

    Update BinarySearchTreeInterface.java

--- BinaryTrees/BinarySearchTreeInterface.java
+++ BinaryTrees/BinarySearchTreeInterface.java
@@ -1,1 +1,17 @@
+public interface BinarySearchTreeInterface<T>{
+  public void clear();
 
+  public boolean isEmpty();
+
+  public boolean contains(T value);
+
+  public T findMin();
+
+  public T findMax();
+
+  public void add(T data);
+
+  public void delete(T data);
+
+  public void printTree();
+}
